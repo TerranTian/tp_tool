b5a2d47c476b93d2463750c1a17111b0080fce3c
terran tian <terran.tian@foxmail.com>
2021-03-10 22:52:04 +0800

    sync:update

--- src/mangle/MangleCmd.ts
+++ src/mangle/MangleCmd.ts
@@ -17,42 +17,54 @@ domprops.forEach(v => dropMap[v] = 1);
 let whiteMap = {};
 whiteList.forEach(v => whiteMap[v] = 1);
 
-let input: string = null;
+let inputs: string[] = [];
 let out: string = null;
 let regex: string = null;
 let resolved: { [key: string]: number } = {};
 let nameCache: string = null;
-let nameCacheMap = {};
+
+
+let cache_data = {
+    nameCacheMap:{},
+    collisionMap:{},
+    literalMap:{}
+};
 
 while (parameters.length > 0) {
     let value = parameters.shift();
     switch (value) {
         case "-i":
         case "--input":
-            input = parameters.shift();
+            inputs = [parameters.shift()];
             break;
         case "-o":
         case "--output":
-            out = parameters.shift() || input;
+            out = parameters.shift();
             break;
         case "--regex":
             regex = parameters.shift() || null;
             break
         case "--resolved":
-            let value = parameters.shift() || "";
-            value.split(",").forEach(v => resolved[v] = 1)
+            {
+                let value = parameters.shift() || "";
+                value.split(",").forEach(v => resolved[v] = 1)
+            }
             break
         case "--nameCache":
             nameCache = parameters.shift();
-            nameCache && (nameCacheMap = JSON.parse(FileUtil.readString(nameCache)) || {});
+            nameCache && (
+                Object.assign(cache_data,JSON.parse(FileUtil.readString(nameCache)))
+                );
             break
         default:
+            inputs.push(value);
             break;
     }
 }
-out = out || input;
+out = out || inputs[0];
+console.log(inputs);
 
-if (!input) {
+if (inputs.length == 0) {
     let message = `
     wrong arguments:
     -i/--input: js to mangle
@@ -67,26 +79,10 @@ if (!input) {
 
 var falafel = require('falafel');
 
-let nameMap = nameCacheMap;
-let value = 0;
-let content: string = FileUtil.readString(input);
-// content = content.replace(/\[["'](\w+)["']\]/g, (str, name) => {
-//     return "." + name;
-// })
+let nameMap = cache_data.nameCacheMap;
+let collisionMap = cache_data.collisionMap;
+let literalMap = cache_data.literalMap;
 
-let collisionMap = {};
-falafel(content,{ecmaVersion:6} ,function (node) {
-    if (node.type == "Identifier") {
-        collisionMap[node.name] = 1;
-    }else if(node.type == "Literal"){
-        let v = node.source();
-        if(v[0] == '"'){
-            v= v.substr(1,v.length-2);//rm "
-            whiteMap[v] = 1;
-            console.log("add",v,v.length);
-        }
-    }
-})
 for (let key in nameMap) {
     collisionMap[nameMap[key]] = 1;
 }
@@ -95,37 +91,65 @@ let keyWords=[
     "if","while","for","else","let","var","const","function","class","number","boolean","NaN","void","undifined","string","break","default","return","case","call","apply","switch","do","of","in","continue","true","false"
 ]
 keyWords.forEach(v=>collisionMap[v] = 1);
-// Object.assign(collisionMap,nameMap)
 
+let value = 0;
 
-var output = falafel(content,{ecmaVersion:6} , function (node) {
-    // console.log(node.type, node.source());
-    if (node.type == "Identifier") {
-        // console.log(node.source(), node.name)
-        var name: string = node.name;
-        if (dropMap[name]) return;
-        if (whiteMap[name]){
-            console.log("white",name);
-            return;
-        };
+function canUse(name){
+    if(collisionMap[name]) return false;
+    if(dropMap[name]) return false;
+    if(whiteMap[name]) return false;
+    if(literalMap[name]) return false;
 
-        if (resolved[name]) return;
-        if (name.length <= 2) return;
+    return true;
+}
 
-        if (!regex || new RegExp(regex).test(name)) {
-            let newName = nameMap[name];
-            if (!newName) {
-                while (true) {
-                    newName = Tiny.encode(value++);
-                    if (!collisionMap[newName]) break;
-                }
-                nameMap[name] = newName;
+inputs.forEach((input,index)=>{
+    let content: string = FileUtil.readString(input);
+    falafel(content,{ecmaVersion:6} ,function (node) {
+        if (node.type == "Identifier") {
+            collisionMap[node.name] = 1;
+        }else if(node.type == "Literal"){
+            let v:string = node.source();
+            if(v[0] == '"'){
+                v= v.substr(1,v.length-2);//rm "
+                literalMap[v] = 1;
+                v.replace(/\w+/ig,(w) => {
+                        literalMap[w] = 1;
+                        return w;
+                    });
             }
-
-            node.update(newName);
         }
-    }
+    })
 });
+inputs.forEach((input,index)=>{
+    let content: string = FileUtil.readString(input);
+    var output = falafel(content,{ecmaVersion:6} , function (node) {
+        // console.log(node.type, node.source());
+        if (node.type == "Identifier") {
+            // console.log(node.source(), node.name)
+            var name: string = node.name;
+            if (dropMap[name]) return;
+            if (whiteMap[name])return;
+            if(literalMap[name])return;
+    
+            if (resolved[name]) return;
+            if (name.length <= 2) return;
+    
+            if (!regex || new RegExp(regex).test(name)) {
+                let newName = nameMap[name];
+                if (!newName) {
+                    while (true) {
+                        newName = Tiny.encode(value++);
+                        if (canUse(newName)) break;
+                    }
+                    nameMap[name] = newName;
+                }
+    
+                node.update(newName);
+            }
+        }
+    });
+    FileUtil.writeString(index==0?out:input, output);
+})
 
-nameCache && (FileUtil.writeString(nameCache, JSON.stringify(nameCacheMap)));
-FileUtil.writeString(out, output);
+nameCache && (FileUtil.writeString(nameCache, JSON.stringify(cache_data)));
